34376fecc9a288f12dd0c5b1d6ea008f120e71fa
eithiri <eithiritun1997@gmail.com>
2022-01-24 15:29:02 +0630

    feat: 15: Handle exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,13 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -38,4 +42,30 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+     /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'errors' => 'Product Model not found'
+                ],Response::HTTP_NOT_FOUND);
+            }
+
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'errors' => 'Incorect route'
+                ],Response::HTTP_NOT_FOUND);
+            }
+        }
+        return parent::render($request, $exception);
+    }
+
 }
