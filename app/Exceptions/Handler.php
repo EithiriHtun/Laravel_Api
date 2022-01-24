7f590aff0d322337c030de270b2092549c1a896f
eithiri <eithiritun1997@gmail.com>
2022-01-24 15:35:20 +0630

    feat: 17:Customized Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,48 +2,77 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+// use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+// use Throwable;
+// use Illuminate\Database\Eloquent\ModelNotFoundException;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use App\Exceptions\ExceptionTrait;
 
 class Handler extends ExceptionHandler
 {
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+    // /**
+    //  * A list of the exception types that are not reported.
+    //  *
+    //  * @var array<int, class-string<Throwable>>
+    //  */
+    // protected $dontReport = [
+    //     //
+    // ];
+
+    // /**
+    //  * A list of the inputs that are never flashed for validation exceptions.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $dontFlash = [
+    //     'current_password',
+    //     'password',
+    //     'password_confirmation',
+    // ];
+
+    // /**
+    //  * Register the exception handling callbacks for the application.
+    //  *
+    //  * @return void
+    //  */
+    // public function register()
+    // {
+    //     $this->reportable(function (Throwable $e) {
+    //         //
+    //     });
+    // }
+
+    //  /**
+    //  * Render an exception into an HTTP response.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \Exception  $exception
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function render($request, Exception $exception)
+    // {
+    //     if ($request->expectsJson()) {
+    //         if ($exception instanceof ModelNotFoundException) {
+    //             return response()->json([
+    //                 'errors' => 'Product Model not found'
+    //             ],Response::HTTP_NOT_FOUND);
+    //         }
+
+    //         if ($exception instanceof NotFoundHttpException) {
+    //             return response()->json([
+    //                 'errors' => 'Incorect route'
+    //             ],Response::HTTP_NOT_FOUND);
+    //         }
+    //     }
+    //     return parent::render($request, $exception);
+    // }
+
+    use ExceptionTrait;
 
     /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
-
-     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -53,17 +82,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if ($request->expectsJson()) {
-            if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Product Model not found'
-                ],Response::HTTP_NOT_FOUND);
-            }
-
-            if ($exception instanceof NotFoundHttpException) {
-                return response()->json([
-                    'errors' => 'Incorect route'
-                ],Response::HTTP_NOT_FOUND);
-            }
+            return $this->apiException($request,$exception);
         }
         return parent::render($request, $exception);
     }
